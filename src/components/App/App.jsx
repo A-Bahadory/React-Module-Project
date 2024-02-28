87c67c7ccf17fa711af59f7ba14fb8f485d43bfa
Marcus <marcus.ats94@gmail.com>
2024-02-28 23:10:34 +0000

    Completed the deck task.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import Footer from "@/components/Footer/Footer";
 import "./App.scss";
 import AppHeader from "../AppHeader/AppHeader.jsx";
 import Restaurant from "@/components/Restaurant/Restaurant.jsx";
+import Deck from "../Deck/Deck";
 
 const App = () => (
   <div>
     <AppHeader />
+    <Deck />
     <Bookings />
     <Restaurant />
     <Footer />
--- src/components/Deck/Deck.jsx
+++ src/components/Deck/Deck.jsx
@@ -1,11 +1,13 @@
-// import Card from "@/components/Card/Card";
-// import "./Deck.scss";
-// import cardsData from "@/data/fakeCards.json";
+import Card from "@/components/Card/Card";
+import "./Deck.scss";
+import cardsData from "@/data/fakeCards.json";
 
-// const Deck = () => {
-// you will need to map over the cardsData array and render a Card component for each card object
-// how will you pass props to the Card component?
+const Deck = () => {
+  return cardsData.map((cardData) => {
+    return (
+      <Card title={cardData.title} url={cardData.url} image={cardData.image} />
+    );
+  });
+};
 
-// };
-
-// export default Deck;
+export default Deck;
