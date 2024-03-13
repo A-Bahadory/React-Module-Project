f3bcee04d280603342484f0600c27cb0f0167ce1
Tajudeen Oyehan <oyehan.uob@gmail.com>
2024-03-13 12:17:49 +0000

    CustomerProfile component created and SearchResult component updated with the customerprofile

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import dayjs from "dayjs";
+import CustomerProfile from "../CustomerProfile/CustomerProfile";
 
 function SearchResults({ results }) {
   const [selectedRow, setSelectedRow] = useState(null);
+  const [id, setCustomerProfile] = useState(null);
 
   const handleRowClick = (index) => {
     if (selectedRow === index) {
@@ -13,55 +15,68 @@ function SearchResults({ results }) {
   };
 
   return (
-    <table>
-      <thead>
-        <tr style={{ backgroundColor: "green" }}>
-          <th>Id</th>
-          <th>Title</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Room Id</th>
-          <th>Check In</th>
-          <th>Check Out</th>
-          <th>Number of Nights</th>
-        </tr>
-      </thead>
-      <tbody>
-        {results.map((item, index) => {
-          const startDate = dayjs(item.checkInDate);
-          const endDate = dayjs(item.checkOutDate);
-          const daysDifference = endDate.diff(startDate, "day");
+    <>
+      <table>
+        <thead>
+          <tr style={{ backgroundColor: "green" }}>
+            <th>Id</th>
+            <th>Title</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Room Id</th>
+            <th>Check In</th>
+            <th>Check Out</th>
+            <th>Number of Nights</th>
+            <th>Customer Profile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results.map((item, index) => {
+            const startDate = dayjs(item.checkInDate);
+            const endDate = dayjs(item.checkOutDate);
+            const daysDifference = endDate.diff(startDate, "day");
 
-          const isSelected = selectedRow === index;
-          const rowStyle = {
-            textAlign: "center",
-            backgroundColor: "blue",
-          };
-          if (isSelected) {
-            rowStyle.backgroundColor = "red";
-          }
+            const isSelected = selectedRow === index;
+            const rowStyle = {
+              textAlign: "center",
+              backgroundColor: "blue",
+            };
+            if (isSelected) {
+              rowStyle.backgroundColor = "red";
+            }
 
-          return (
-            <tr
-              key={index}
-              style={rowStyle}
-              onClick={() => handleRowClick(index)}
-            >
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-              <td>{item.firstName}</td>
-              <td>{item.surname}</td>
-              <td>{item.email}</td>
-              <td>{item.roomId}</td>
-              <td>{item.checkInDate}</td>
-              <td>{item.checkOutDate}</td>
-              <td>{daysDifference}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            return (
+              <tr
+                key={index}
+                style={rowStyle}
+                onClick={() => handleRowClick(index)}
+              >
+                <td>{item.id}</td>
+                <td>{item.title}</td>
+                <td>{item.firstName}</td>
+                <td>{item.surname}</td>
+                <td>{item.email}</td>
+                <td>{item.roomId}</td>
+                <td>{item.checkInDate}</td>
+                <td>{item.checkOutDate}</td>
+                <td>{daysDifference}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      setCustomerProfile(item.id);
+                    }}
+                  >
+                    Show profile
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <CustomerProfile id={id} />
+    </>
   );
 }
 
